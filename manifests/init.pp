fd7ce2e1cb73dd32e2ca6850cbfc14c370fc43a2
Mats93 <mats.93@hotmail.com>
2018-10-26 14:40:07 +0200

    Added descriptions on the available parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,70 @@
-# A description of what this class does
+# Puppet module for installing CouchDB.
 #
-# @summary A short summary of the purpose of this class
+# Main class, contains all other classes.
 #
-# @example
-#   include couchdb
+#
+# @param database_dir
+#	Specifies location of CouchDB database files (*.couch named). 
+#	This location should be writable and readable for the user the CouchDB service runs as (couchdb by default).
+#
+#
+# @param file_compression
+#	Method used to compress everything that is appended to database and view index files, except for attachments. 
+#	Available methods are:
+#	  - none:      no compression
+#	  - snappy:    use Google Snappy, a very fast compressor/decompressor
+#	  - deflate_N: use zlib’s deflate; N is the compression level which ranges from 
+#		1 (fastest, lowest compression ratio) to 9 (slowest, highest compression ratio)
+#
+#
+# @param max_dbs_open
+#	This option places an upper bound on the number of databases that can be open at once.
+#
+#
+# @param uuid
+#	Unique identifier for this CouchDB server instance.
+#	This parameter is optional, it will be generetad by the system if none is provided.
+#
+#
+# @param max_document_size
+#	Limit maximum document body size.
+#
+#
+# @param couch_peruser
+#	If set to 'true', couch_peruser ensures that a private per-user database exists for each document in _users.
+#
+#
+# @param default_security
+#	Default security object for databases, available options is:
+#	  - everyone:    Anyone can perform reads and writes.
+#	  - admin_only:  Only admins can read and write.
+#	  - admin_local: Sharded databases can be read and written by anyone but the
+#		shards can only be read and written by admins.
+#
+#
+# @param require_valid_user
+#	When set to 'true', no requests are allowed from anonymous users.
+#	Everyone must be authenticated.
+#
+#
+# @param bind_address
+#	Defines the IP address by which the clustered port is available.
+#
+#
+# @param port
+#	Defines the port number to listen.
+#
+#
+# @param admin_password
+#	Enables the admin account and sets the admin password.
+#	This crashes the 'admin party' (everyone is a admin).
+#	
+#
+# @param allow_persistent_cookies
+#	Makes cookies persistent if set to 'true'. 
+#
+
 class couchdb {
+
+
 }
