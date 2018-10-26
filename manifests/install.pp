7fe0f71fe475ad1a39238fbb9dc46e507247fb5c
Mats93 <mats.93@hotmail.com>
2018-10-26 19:32:53 +0200

    Added untested intsall.pp file

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,47 @@
-# A description of what this class does
-#
-# @summary A short summary of the purpose of this class
-#
-# @example
-#   include couchdb::install
+# This class installs or creates new objects on the system.
+
 class couchdb::install {
+
+  # Includes apt module.
+  include apt
+
+  # Creates the database directory
+  file { $couchdb::database_dir:
+    ensure => 'directory',
+  }
+
+  # Adds the repository of couchdb.
+  apt::source { 'apache_couchdb':
+    location => 'https://apache.bintray.com/couchdb-deb',
+    repos    => 'main',
+    include  => {
+      'deb' => true,
+    },
+    before => Package['curl'],
+  }
+
+  # Installs curl.
+  package { 'curl':
+    ensure => installed,
+    before => Exec['add_key'],
+  }
+
+# Adds the PPA key.
+  exec {'add_key':
+    command => 'curl -L https://couchdb.apache.org/repo/bintray-pubkey.asc  | sudo apt-key add -',
+    path    => ['/usr/bin/'],
+    require => Package['curl'],
+    before  => Exec['update_apt'],
+  }
+
+# Updates all repositories.
+  exec {'update_apt':
+    command => '/usr/bin/apt-get update',
+    before  => Package['couchdb'],
+  }
+
+  # Installs the couchdb package.
+  package { 'couchdb':
+    ensure   => installed,
+  }
 }
