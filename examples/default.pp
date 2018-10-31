d014759651dacfa2d125f1ac36f7f5c9b62f2570
mats <moskjaer@stud.ntnu.no>
2018-11-01 00:05:45 +0100

    Removed old testing files, and added examples of use

---
+++ examples/default.pp
@@ -0,0 +1,5 @@
+# Installs the couchdb with all its default parameters.
+
+node default {
+  class {'couchdb':}
+}
--- examples/init.pp
+++ examples/init.pp
@@ -1,2 +1,15 @@
-include couchdb
+# Example of use with parameters.
+#
+# Specify the folder to hold the couchDB data, this folder wil be created.
+# Allows couchDB to listen on all addresses.
+# Binds the port to 5050
+# Creates a admin password, in doing so it removes 'admin party'.
 
+node default {
+  class {'couchdb':
+    database_dir   => '/data',
+    bind_address   => '0.0.0.0',
+    port           => 5050,
+    admin_password => 'MySecretPassword',
+  }
+}
